dbcb4cb146a8642e6fa6d0bd992dd0bb707d8d56
Agata Tekien <tekienagata@gmail.com>
2024-04-18 21:49:18 +0200

    Exercise 3 done

--- L1Ex3.py
+++ L1Ex3.py
@@ -20,3 +20,58 @@ class Dice:
             return f"Sides = {self.__sides}, " f"value = {self._value}"
         else:
             return f"Sides = {self.__sides}, " f"value not set"
+
+
+class BlackJack:
+    computer_result = 0
+    player_result = 0
+    c1, c2, p1, p2 = Dice(6), Dice(6), Dice(6), Dice(6)
+
+    def game(self):
+        if self.player_result > 21:
+            return self.who_wins()
+        if self.computer_result < 17:
+            self.throw_computer()
+        if 0 <= self.player_result:
+            answer = input("Do you want to roll the dice?")
+            if answer.strip().lower() in ["yes", "y"]:
+                self.throw_player()
+            else:
+                self.player_result = (
+                    -1 if self.player_result == 0 else (-1) * self.player_result
+                )
+        elif self.computer_result >= 17:
+            return self.who_wins()
+        print(
+            f"Computer: {self.computer_result}, player: {0 if self.player_result == -1 else abs(self.player_result)}"
+        )
+        self.game()
+
+    def throw_computer(self):
+        self.c1.roll()
+        self.c2.roll()
+        self.computer_result += self.c1.get_value() + self.c2.get_value()
+
+    def throw_player(self):
+        self.p1.roll()
+        self.p2.roll()
+        self.player_result += self.p1.get_value() + self.p2.get_value()
+
+    def who_wins(self):
+        print(
+            f"Computer: {self.computer_result}, player: {0 if self.player_result == -1 else abs(self.player_result)}"
+        )
+        if self.player_result > 21:
+            return "computer wins"
+        if self.player_result == self.computer_result:
+            return "it's a tie"
+        if self.player_result == 21 or self.computer_result == 21:
+            "Blackjack!\n"
+            if self.player_result == 21:
+                return "player wins"
+            else:
+                return "computer wins"
+        if self.player_result < 21 and 21 > self.computer_result > self.player_result:
+            return "computer wins"
+        else:
+            return "player wins"
--- tests/test_L1Ex3.py
+++ tests/test_L1Ex3.py
@@ -1,17 +1,18 @@
-# import pytest
-from L1Ex3 import Dice
+import pytest
+
+from L1Ex3 import BlackJack, Dice
 
 
 def test_dice():
     d1 = Dice(6)
-    assert d1._Dice__sides == 6
+    assert d1.get_sides() == 6
     assert d1._value is None
 
 
 def test_roll():
     d1 = Dice(6)
     d1.roll()
-    assert d1._value in [1, 2, 3, 4, 5, 6]
+    assert d1.get_value() in [1, 2, 3, 4, 5, 6]
 
 
 def test_get_sides():
@@ -24,6 +25,36 @@ def test_get_value():
     assert d1.get_value() is None
 
 
-def test_str(capsys):
+def test_str():
     d1 = Dice(6)
     assert str(d1).strip() == "Sides = 6, value not set"
+
+
+def test_blackjack_computer():
+    testing = BlackJack()
+    testing.throw_computer()
+    assert testing.computer_result > 1
+
+
+def test_blackjack_player(monkeypatch):
+    mock_answer = ["no", "yes"]
+    monkeypatch.setattr("builtins.input", lambda _: mock_answer.pop())
+    testing = BlackJack()
+    testing.game()
+    assert testing.player_result < 0 or testing.player_result >= 21
+
+
+@pytest.mark.parametrize(
+    "computer, player, outcome",
+    [
+        (21, 21, "it's a tie"),
+        (18, 12, "computer wins"),
+        (17, 19, "player wins"),
+        (17, 23, "computer wins"),
+    ],
+)
+def test_who_wins(computer, player, outcome):
+    testing = BlackJack()
+    testing.computer_result = computer
+    testing.player_result = player
+    assert testing.who_wins() == outcome
